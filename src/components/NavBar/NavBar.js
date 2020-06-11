7656fc88742b4715e5bd4e381b7b48a253ef11f4
wenner <nelsondiasdemedeiros@gmail.com>
2020-06-11 12:16:57 -0300

    chore: add component NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './navbar.css';
+
+const NavBar = () => {
+  return (
+    <ul className="nav">
+      <li><a href="home"><i className="fa fa-home"></i>Home</a></li>
+      <li><a href="home"><i className="fa fa-user"></i>About</a></li>
+      <li><a href="home"><i className="fa fa-list"></i>Services</a></li>
+      <li><a href="home"><i className="fa fa-briefcase"></i>Portfolio</a></li>
+      <li><a href="home"><i className="fa fa-envelope"></i>Blog</a></li>
+      <li><a href="home"><i className="fa fa-comments"></i>Contact</a></li>
+    </ul>
+  )
+}
+
+export default NavBar;
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './sidebar.css';
 
+import NavBar from '../NavBar/NavBar';
+
 const SideBar = () => {
   return (
     <div className="sidebar">
       <div className="logo">
         <a href="/">Wenner</a>
       </div>
-      <ul className="nav">
-        <li><a href="home"><i className="fa fa-home"></i>Home</a></li>
-        <li><a href="home"><i className="fa fa-user"></i>About</a></li>
-        <li><a href="home"><i className="fa fa-list"></i>Services</a></li>
-        <li><a href="home"><i className="fa fa-briefcase"></i>Portfolio</a></li>
-        <li><a href="home"><i className="fa fa-envelope"></i>Blog</a></li>
-        <li><a href="home"><i className="fa fa-comments"></i>Contact</a></li>
-      </ul>
+      <NavBar />
       <div className="copyright-text">
         &copy; 2020 Nelson Wenner
       </div>
