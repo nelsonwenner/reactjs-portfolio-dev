d685abf6a6ec5faffdca23b6113ae615b434a7df
wenner <nelsondiasdemedeiros@gmail.com>
2020-06-11 14:19:02 -0300

    chore: add effects in itens from SideBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import './navbar.css';
 const NavBar = () => {
   return (
     <ul className="nav">
-      <li><a href="home"><i className="fa fa-home"></i>Home</a></li>
+      <li><a href="home" className="active"><i className="fa fa-home"></i>Home</a></li>
       <li><a href="home"><i className="fa fa-user"></i>About</a></li>
       <li><a href="home"><i className="fa fa-list"></i>Services</a></li>
       <li><a href="home"><i className="fa fa-briefcase"></i>Portfolio</a></li>
