9ed513fdc35daa9753beb51f2756d18010a2ed02
wenner <nelsondiasdemedeiros@gmail.com>
2020-06-11 12:13:15 -0300

    chore: add logo

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,7 +4,20 @@ import './sidebar.css';
 const SideBar = () => {
   return (
     <div className="sidebar">
-      <h1>SideBar</h1>
+      <div className="logo">
+        <a href="/">Wenner</a>
+      </div>
+      <ul className="nav">
+        <li><a href="home"><i className="fa fa-home"></i>Home</a></li>
+        <li><a href="home"><i className="fa fa-user"></i>About</a></li>
+        <li><a href="home"><i className="fa fa-list"></i>Services</a></li>
+        <li><a href="home"><i className="fa fa-briefcase"></i>Portfolio</a></li>
+        <li><a href="home"><i className="fa fa-envelope"></i>Blog</a></li>
+        <li><a href="home"><i className="fa fa-comments"></i>Contact</a></li>
+      </ul>
+      <div className="copyright-text">
+        &copy; 2020 Nelson Wenner
+      </div>
     </div>
   )
 }
