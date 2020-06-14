2d6d80bf231ae494b36fe7f077f70b6d651d4455
wenner <nelsondiasdemedeiros@gmail.com>
2020-06-14 17:29:17 -0300

    refactor: componentizing some htmls

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import './main.css';
 
-import Profile from '../../assets/image/nelsonwenner.jpeg';
+import Profile from '../Profile/Profile';
+import Service from '../Service/Service';
 import About from '../About/About';
 
 const Main = () => {
   return (
-    <>
-      <div className="main">
-        <div className="container-main">
-          <div className="intro">
-            <img src={ Profile } alt="profile" className="shadow-dark" />
-            <h1>Nelson Wenner</h1>
-            <p>I'm a FullStack Web Developer</p>
-            <div className="social-links">
-              <a href="/"><i className="fa fa-twitter"></i></a>
-              <a href="/"><i className="fa fa-facebook"></i></a>
-              <a href="/"><i className="fa fa-instagram"></i></a>
-              <a href="/"><i className="fa fa-linkedin"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="container-main">
+      <Profile />
       <About />
-    </>
+      <Service />
+    </div>
   )
 }
 
---
+++ src/components/Service/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './service.css';
+
+const Service = () => {
+  return (
+    <div className="service">
+      <div className="container-service">
+        <div className="row">
+          <div className="section-title">
+            <h2>Services</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Service;
